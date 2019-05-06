a9fa1ccf9b00ad2c6be3378024de45ce695958bd
Alex Curci <alex.curci@gmail.com>
2019-05-06 15:57:04 -0400

    Added update clause in Linux vim update

--- deploy.sh
+++ deploy.sh
@@ -41,24 +41,32 @@ fi
 # Update for Linux
 if [[ $os_system == "linux"* ]]
 then
-    cd ~
-    sudo apt install build-essential cmake python3-dev
-    git clone https://github.com/vim/vim.git
-    cd vim
-    ./configure --with-features=huge \
-        --enable-multibyte \
-        --enable-rubyinterp=yes \
-        --enable-python3interp=yes \
-        --with-python3-config-dir=/usr/lib/python3.5/config \
-        --enable-perlinterp=yes \
-        --enable-luainterp=yes \
-        --enable-gui=gtk2 \
-        --enable-cscope \
-        --prefix=/usr/local
-    make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
-    cd ~/vim
-    make
-    sudo make install
+  versionNumber="$(vim --version | awk 'NR==1 { print $5 }')"
+  if [[ $versionNumber > 8.0 ]]
+    then
+        echo
+        echo "Vim is up to date."
+        echo
+    else
+        cd ~
+        sudo apt install build-essential cmake python3-dev
+        git clone https://github.com/vim/vim.git
+        cd vim
+        ./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --prefix=/usr/local
+        make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
+        cd ~/vim
+        make
+        sudo make install
+    fi
 fi
 echo
 echo "------ Vim Updated ------"
